ca560dc738faf92f80d2fc8d1f9b38fc73974dc9
Kevin Zhu <kevin_zhu@college.harvard.edu>
2018-01-10 17:13:24 -0500

    changed the bat file that starts everything with one command

--- startBothEnds.bat
+++ startBothEnds.bat
@@ -2,7 +2,7 @@ cd "C:\Program Files\MongoDB\Server\3.6\bin"
 START mongod --dbpath "C:\Users\kjzhu\Documents\mongodb\data\db" --port 5000
 TIMEOUT 1
 cd "C:\Users\kjzhu\Documents\Harvard\dev-bootcamp\dorm-supplies"
-nodemon app.js
+START nodemon app.js
 TIMEOUT 1
 cd "C:\Users\kjzhu\Documents\Harvard\dev-bootcamp\dorm-supplies-frontend"
 npm run dev
